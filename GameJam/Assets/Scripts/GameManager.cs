eac66f81c05b8a79b9b3b2befe53d5ea968f22bf
Tim Peeters <tim.peeters12345@gmail.com>
2019-04-26 13:50:58 +0200

    New Scripts

--- GameJam/Assets/Scripts/GameManager.cs
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] int currentPlayer;
+
+    bool doingTurn;
+
+    public GameObject selectedShip;
+    Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -15,4 +22,14 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    void SelectShip()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+
+    }
+
+
 }
--- GameJam/Assets/Scripts/TurnbasedSpaceship.cs
+++ GameJam/Assets/Scripts/TurnbasedSpaceship.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class TurnbasedSpaceship : MonoBehaviour
 {
-
+    GameManager mgr;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mgr = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
