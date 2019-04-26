bdc24376e19ba92ee1433b58ac2d0e63177c86a8
JHunter465 <jael.vanrossum@student.hku.nl>
2019-04-26 16:44:28 +0200

    FinishedGameReadyformaster

--- GameJam/Assets/Scripts/MainMenu.cs
+++ GameJam/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
         public void NewGameBTN(string newGameLevel)
         {
-            SceneManager.LoadScene(newGameLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         public void QuitGameBTN()
         {
